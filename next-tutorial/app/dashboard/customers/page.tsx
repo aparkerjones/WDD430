1ec2201db68e4b9d8d553c848ce7f847dcb19f94
aparkerjones <parkor.j@gmail.com>
2026-07-16 14:33:59 -0600

    Vercel testing

--- next-tutorial/app/dashboard/customers/page.tsx
+++ next-tutorial/app/dashboard/customers/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import type { Metadata } from 'next';
+import Search from '@/app/ui/search';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export const dynamic = 'force-dynamic';
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl">Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
